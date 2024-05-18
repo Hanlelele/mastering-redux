b197faf0d4e63edddf831181cb13260477285d14
21120064 <lequochan2003@gmail.com>
2024-05-18 22:30:33 +0700

    React Hooks Section 3

--- reaction/src/components/App.js
+++ reaction/src/components/App.js
@@ -1,18 +1,35 @@
-import { useReducer } from 'react'
+import { useEffect, useReducer } from 'react'
 import reducer, { initialState } from "../state/reducer";
 import PublishMessage from './PublishMessage';
+import PubSub from '../pubnub';
 import MessageBoard from './MessageBoard';
 // eslint-disable-next-line
 import context from '../context';
+import SetUsername from './SetUsername';
+
+const pubsub = new PubSub();
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  useEffect(() => {
+    pubsub.addListener({
+      message: messageObject => {
+        const { channel, message } = messageObject;
+    
+        console.log('Received message', message, 'channel', channel);
+    
+        dispatch(message)
+      }
+    });
+  }, []);
+
   console.log('state', state);
 
   return (
-    <context.Provider value={{ state, dispatch }} className="App">
+    <context.Provider value={{ state, dispatch, pubsub}} className="App">
       <h2>Reaction</h2>
+      <SetUsername />
       <hr />
       <PublishMessage />
       <hr />
--- reaction/src/components/MessageBoard.js
+++ reaction/src/components/MessageBoard.js
@@ -1,18 +1,24 @@
 import { useAppContext } from "./hooks";
+import CreateReaction from "./CreateReaction";
+import MessageReactions from "./MessageReactions";
+
 
 function MessageBoard() {
-    const { state: { messages } } = useAppContext();
+    const { state: { messages, reactionsMap } } = useAppContext();
 
     return ( 
         <div>
             {
                 messages.map(messageItem => {
-                    const { id, text, timestamp } = messageItem;
+                    const { id, text, username, timestamp } = messageItem;
 
                     return (
                         <div key={id}>
                             <h4>{new Date(timestamp).toLocaleString()}</h4>
                             <p>{text}</p>
+                            <h4>- {username}</h4>
+                            <CreateReaction messageId={id} />
+                            <MessageReactions messageReactions={reactionsMap[id]}/>
                             <hr />
                         </div>
                     )
